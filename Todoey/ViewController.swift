74276d6e69eeb221a8a50216bf3770614d87132f
Mykyta Havrylenko <gavverment@gmail.com>
2022-07-26 18:58:23 +0300

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Mykyta Havrylenko on 26.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
